0ea10561ff64ef02bacbfcfc65ba6e116e5d2b68
Rostam Ali <mac@macs-Mac-mini.local>
2026-07-01 19:38:51 +0600

    Adding legacy import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
     plugins: [
