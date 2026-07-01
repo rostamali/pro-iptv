3148cf1b364dbe173de0a93331efd967f1ab3e45
Rostam Ali <mac@macs-Mac-mini.local>
2026-07-01 19:36:55 +0600

    Adding legacy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        legacy({
+            targets: ['defaults', 'not IE 11'],
+        }),
+    ],
     server: {
         proxy: {
             '/proxy': {
