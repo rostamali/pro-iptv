0ed16e2386fc440c6e6cff4cea007fcbbf9f6721
Rostam Ali <mac@macs-Mac-mini.local>
2026-07-01 21:25:41 +0600

    Fixxing legacy issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
-        legacy({
-            targets: ['defaults', 'not IE 11'],
-        }),
     ],
     server: {
         proxy: {
